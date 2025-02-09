3940f8251efaf30ce142aaa96842ec4e30ba4e9e
liudmylasovetovs <milkaegik@gmail.com>
2025-02-08 20:53:09 -0800

    docs

---
+++ docs/index.rst
@@ -0,0 +1,103 @@
+.. Final Progect documentation master file, created by
+   sphinx-quickstart on Sat Feb  8 20:31:33 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Final Progect documentation
+===========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Final Progect main
+===========================
+
+.. automodule:: main
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Final Progect API Auth
+===========================
+
+.. automodule:: src.api.auth
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Final Progect API Contacts
+===========================
+
+.. automodule:: src.api.contacts
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Final Progect API Utils
+===========================
+
+.. automodule:: src.api.utils
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+
+Final Progect Database Models
+=============================
+
+.. automodule:: src.database.models
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+
+Final Progect repository Contacts
+=================================
+
+.. automodule:: src.repository.contacts
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Final Progect repository Users
+==============================
+
+.. automodule:: src.repository.users
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Final Progect Services Auth
+===========================
+.. automodule:: src.services.auth
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Final Progect Services Contacts
+===============================
+
+.. automodule:: src.services.contacts
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+Final Progect Services Users
+============================
+
+.. automodule:: src.services.users
+  :members:
+  :undoc-members:
+  :show-inheritance:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
---
+++ tests/test_unit_repository_contacts.py
@@ -0,0 +1,227 @@
+from datetime import date, timedelta
+from pprint import pprint
+from unittest.mock import AsyncMock, MagicMock
+
+import pytest
+from conftest import test_user
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.models import Contact, User
+from src.repository.contacts import ContactRepository
+from src.schemas.contacts import ContactBase
+from tests.conftest import TestingSessionLocal
+
+test_contacts = [
+    {
+        "first_name": "first1",
+        "last_name": "last1",
+        "email": "user2@mail.com",
+        "phone_number": "0676650154",
+        "birthday": str(date(1984, 12, 13)),
+        "additional_data": "additional_data1",
+    },
+    {
+         "first_name": "first2",
+        "last_name": "last2",
+        "email": "user3@mail.com",
+        "phone_number": "0676826627",
+        "birthday": str(date(1956, 12, 17)),
+        "additional_data": "additional_data2",
+    },
+]
+
+
+@pytest.fixture
+def mock_session():
+    mock_session = AsyncMock(spec=AsyncSession)
+    return mock_session
+
+
+@pytest.fixture
+def contact_repository(mock_session):
+    return ContactRepository(mock_session)
+
+
+@pytest.fixture
+def user():
+    return User(id=1, username=test_user["username"], email=test_user["email"])
+
+
+@pytest.mark.asyncio
+async def test_get_contacts(contact_repository, mock_session, user):
+    mock_result = MagicMock()
+
+    contacts_to_get = [
+        Contact(
+            id=i + 1,
+            first_name=contact["first_name"],
+            last_name=contact["last_name"],
+            email=contact["email"],
+            phone_number=contact["phone_number"],
+            birthday=contact["birthday"],
+            additional_data=contact["additional_data"],
+            user=user,
+        )
+        for i, contact in enumerate(test_contacts[:2])
+    ]
+
+    mock_result.scalars.return_value.all.return_value = contacts_to_get
+    mock_session.execute = AsyncMock(return_value=mock_result)
+
+    contacts = await contact_repository.get_contacts(skip=0, limit=10, user=user)
+
+    assert len(contacts) == 2
+    assert contacts[0].first_name == test_contacts[0]["first_name"]
+    assert contacts[0].last_name == test_contacts[0]["last_name"]
+    assert contacts[0].email == test_contacts[0]["email"]
+    assert contacts[0].phone_number == test_contacts[0]["phone_number"]
+    assert contacts[0].birthday == test_contacts[0]["birthday"]
+    assert contacts[0].additional_data == test_contacts[0]["additional_data"]
+    assert contacts == contacts_to_get
+
+
+@pytest.mark.asyncio
+async def test_get_contacts_by_id(contact_repository, mock_session, user):
+    mock_result = MagicMock()
+    contacts_to_get = [
+        Contact(
+            id=i + 1,
+            first_name=contact["first_name"],
+            last_name=contact["last_name"],
+            email=contact["email"],
+            phone_number=contact["phone_number"],
+            birthday=contact["birthday"],
+            additional_data=contact["additional_data"],
+            user=user,
+        )
+        for i, contact in enumerate(test_contacts[:2])
+    ]
+    mock_result.scalar_one_or_none.return_value = contacts_to_get[0]
+    mock_session.execute = AsyncMock(return_value=mock_result)
+
+    contact = await contact_repository.get_contacts_by_id(contact_id=1, user=user)
+
+    assert contact is not None
+    assert contact.id == 1
+    assert contact.first_name == test_contacts[0]["first_name"]
+
+
+@pytest.mark.asyncio
+async def test_create_contact_existing_user(contact_repository, mock_session, user):
+    body = ContactBase(
+        first_name=test_contacts[0]["first_name"],
+        last_name=test_contacts[0]["last_name"],
+        email=test_contacts[0]["email"],
+        phone_number=test_contacts[0]["phone_number"],
+        birthday=test_contacts[0]["birthday"],
+        additional_data=test_contacts[0]["additional_data"],
+    )
+
+    contact = await contact_repository.create_contact(body, user)
+
+    assert contact.user == user
+    mock_session.add.assert_called_once_with(contact)
+    mock_session.commit.assert_called_once()
+    mock_session.refresh.assert_called_once_with(contact)
+
+
+@pytest.mark.asyncio
+async def test_update_contact(contact_repository, mock_session, user):
+
+    contact_old = Contact(
+        id=1,
+        first_name=test_contacts[0]["first_name"],
+        last_name=test_contacts[0]["last_name"],
+        email=test_contacts[0]["email"],
+        phone_number=test_contacts[0]["phone_number"],
+        birthday=test_contacts[0]["birthday"],
+        additional_data=test_contacts[0]["additional_data"],
+    )
+    contact_new = ContactBase(
+        first_name="updated first name",
+        last_name="updated last name",
+        email="updated@email.com",
+        phone_number="updated phone number",
+        birthday=str(date(2020, 1, 1)),
+        additional_data="updated additional data",
+    )
+    mock_result = MagicMock()
+    mock_result.scalar_one_or_none.return_value = contact_old
+    mock_session.execute = AsyncMock(return_value=mock_result)
+
+    result = await contact_repository.update_contact(
+        contact_id=1, body=contact_new, user=user
+    )
+  
+    assert result is not None
+    assert result.first_name == contact_new.first_name
+    assert result.last_name == contact_new.last_name
+    assert result.email == contact_new.email
+    assert result.phone_number == contact_new.phone_number
+    assert result.birthday == contact_new.birthday
+    assert result.additional_data == contact_new.additional_data
+    mock_session.commit.assert_awaited_once()
+    mock_session.refresh.assert_awaited_once_with(contact_old)
+
+
+@pytest.mark.asyncio
+async def test_remove_contact(contact_repository, mock_session, user):
+    existing_contact = Contact(
+        id=1,
+        first_name=test_contacts[0]["first_name"],
+        last_name=test_contacts[0]["last_name"],
+        email=test_contacts[0]["email"],
+        phone_number=test_contacts[0]["phone_number"],
+        birthday=test_contacts[0]["birthday"],
+        additional_data=test_contacts[0]["additional_data"],
+    )
+    mock_result = MagicMock()
+    mock_result.scalar_one_or_none.return_value = existing_contact
+    mock_session.execute = AsyncMock(return_value=mock_result)
+
+    result = await contact_repository.remove_contact(contact_id=1, user=user)
+   
+    assert result is not None
+    assert result.first_name == existing_contact.first_name
+    assert result.last_name == existing_contact.last_name
+    assert result.email == existing_contact.email
+    assert result.phone_number == existing_contact.phone_number
+    assert result.birthday == existing_contact.birthday
+    assert result.additional_data == existing_contact.additional_data
+    mock_session.delete.assert_awaited_once_with(existing_contact)
+    mock_session.commit.assert_awaited_once()
+
+
+@pytest.mark.asyncio
+async def test_search_contacts_valid_query(
+    contact_repository, mock_session, user, client
+):
+    user = User(id=0)
+    contacts_to_db = [
+        Contact(
+            id=i + 1,
+            first_name=contact["first_name"],
+            last_name=contact["last_name"],
+            email=contact["email"],
+            phone_number=contact["phone_number"],
+            birthday=date.fromisoformat(contact["birthday"]),
+            additional_data=contact["additional_data"],
+            user=user,
+        )
+        for i, contact in enumerate(test_contacts[:2])
+    ]
+    contact_to_search = contacts_to_db[0]
+
+    async with TestingSessionLocal() as session:
+        session.add_all(contacts_to_db)
+        await session.commit()
+
+        contact_repository.db = session
+
+        search_query = contact_to_search.first_name
+        contacts = await contact_repository.search_contacts(
+            search=search_query, skip=0, limit=100, user=user
+        )
+  
+    assert len(contacts) >= 1
+    assert contact_to_search.id in [contact.id for contact in contacts]
